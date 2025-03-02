417656b4c227f6d42fd24ca4e1d841579033ebd8
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-03-02 02:44:17 -0800

    Obstacles moving

---
+++ Dash Of Colour/Assets/Scripts/ObstaclesMoving.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ObstaclesMoving : MonoBehaviour
+{
+    public Vector3 moveDirection = Vector3.back;
+    public float speed = 2.0f;
+    public float moveDistance = 2.4f;
+    private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pingPongValue = Mathf.PingPong(Time.time * speed, moveDistance * 2) - moveDistance;
+        transform.position = startPosition + moveDirection.normalized * pingPongValue;
+    }
+}
