23fcd0139e7a1409b42b967072dfa53040df991a
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-03-02 15:23:08 -0800

    Obstacles add
    More moving obstacles

---
+++ Dash Of Colour/Assets/Scripts/ObstaclesMovingPro.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ObstaclesMovingPro : MonoBehaviour
+{
+    public float speed = 3f;
+    public Vector3[] directions = { Vector3.forward, Vector3.left, Vector3.back, Vector3.right };
+    private int currentDirectionIndex = 0;
+    private float distance = 1f; 
+    private float movedDistance = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.Translate(directions[currentDirectionIndex] * step);
+        movedDistance += step;
+        if (movedDistance >= distance)
+        {
+            movedDistance = 0f;
+            currentDirectionIndex = (currentDirectionIndex + 1) % directions.Length;
+        }
+    }
+}
