0830339f06b4937f73b763b515ad50b70c3c0641
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-03-02 16:48:14 -0800

    Blink effect when respawn

--- Dash Of Colour/Assets/Scripts/ExitResetterScript.cs
+++ Dash Of Colour/Assets/Scripts/ExitResetterScript.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ExitResetterScript : MonoBehaviour
 {
     private Vector3 startPosition;
     private Quaternion startRotation;
     private Rigidbody playerRB;
+
+    private Renderer playerRenderer; 
+    public float blinkDuration = 2f; 
+    public float blinkInterval = 0.2f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerRenderer = GetComponent<Renderer>();
 
-        
         startPosition = transform.position;
         startRotation = transform.rotation;
     }
@@ -41,6 +46,37 @@ public class ExitResetterScript : MonoBehaviour
         {
             playerRB.linearVelocity = Vector3.zero;
             playerRB.angularVelocity = Vector3.zero;
+        }
+
+        StartCoroutine(BlinkEffect());
+    }
+
+    IEnumerator BlinkEffect()
+    {
+        float elapsedTime = 0f;
+        bool isVisible = true;
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        while (elapsedTime < blinkDuration)
+        {
+            foreach (Renderer renderer in renderers)
+            {
+                if (renderer != null)
+                    renderer.enabled = isVisible;
+            }
+
+            isVisible = !isVisible;
+
+            yield return new WaitForSeconds(blinkInterval);
+            elapsedTime += blinkInterval;
+        }
+
+        
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer != null)
+                renderer.enabled = true;
         }
     }
 }
