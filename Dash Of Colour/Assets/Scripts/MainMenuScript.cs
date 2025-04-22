a83908d3e02017a65e1958f4e52a277e076bd997
Tejal-V-Shetty <tvshetty@usc.edu>
2025-04-22 11:54:03 -0700

    Updated Level 4 + Other minor changes

--- Dash Of Colour/Assets/Scripts/MainMenuScript.cs
+++ Dash Of Colour/Assets/Scripts/MainMenuScript.cs
@@ -22,4 +22,8 @@ public class MainMenuScript : MonoBehaviour
     {
         SceneManager.LoadScene("Level 3");
     }
+    public void playLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
 }
